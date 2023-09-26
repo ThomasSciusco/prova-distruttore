b9c5ee7868a0fe065e8dd4aa56602fcbb3eaed30
sciusco.20197 <sciusco.20197@E2-9-PC25>
2023-09-26 13:34:36 +0200

    Aggiungere i file di progetto.

---
+++ prova distruttore/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova_distruttore
+{
+    internal class esempio
+    {
+        private string stringa;
+        private int numero;
+
+        private bool disposed = false;
+            public esempio ()
+        {
+            stringa = ""; 
+            numero = 0;
+        }
+        public void Dispose()
+
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+            if(disposing) 
+            { 
+                
+            }
+            disposed= true;
+        }
+        ~ esempio() 
+        {
+            Dispose(false);
+            Console.WriteLine("risorse gestite");
+        }
+
+        public string Stringa
+        { 
+            get {return stringa;} 
+            set { stringa = value;}
+        }
+        public int Numero
+        { 
+            get { return numero;} 
+            set { numero = value;} 
+        }
+        
+    }
+}
+
---
+++ prova distruttore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using prova_distruttore;
+
+namespace prova_distruttore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           esempio esempio1= new esempio();
+            Console.WriteLine("inserisci stringa: ");
+            esempio1.Stringa = Console.ReadLine();
+
+            Console.WriteLine("inserisci numero: ");
+            esempio1.Numero = int.Parse(Console.ReadLine());
+            esempio1.Dispose();
+        }
+    }
+}
